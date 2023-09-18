5f3691589aa18dda75e93eba1edee8a2d85b0d2d
Your Name <vilasicoding@gmail.com>
2023-09-17 22:37:23 -0400

    Removed Figlet

--- index.js
+++ index.js
@@ -1,44 +1,31 @@
 const robot = require('robotjs');
 const keypress = require('keypress');
-const figlet = require('figlet');
 const colors = require('colors');
-
-figlet("Choad's Custom Clicker!", function (err, data) {
-  if (err) {
-    console.log('Something went wrong...');
-    console.dir(err);
-    return;
-  }
-  console.log(data.green);
-  console.log('Press Ctrl + F To Start Clicking'.green);
-  console.log('Press Ctrl + C to Close Program'.red);
-});
+console.log("Welcome to Choad's Custom Clicker!".green);
+console.log('Ctrl + F to Activate (Only press once!)'.green);
+console.log('Ctrl + C To Quit'.red);
 
 // Make `process.stdin` begin emitting "keypress" events
 keypress(process.stdin);
 
-function randomNum() {
-  return Math.random() * 2000;
-}
-
+let intervalId;
 // Listen for the "keypress" event
 process.stdin.on('keypress', function (ch, key) {
-  let intervalId;
-  console.log(key);
-
   // Detects if Ctrl + F is pressed, and starts the clicking
   if (key) {
     if (key.name === 'f' && key.ctrl === true) {
+      console.log('Clicking Started!'.yellow);
       intervalId = setInterval(() => {
         robot.mouseClick();
         console.log('Click!');
-      }, 2000 + randomNum());
+      }, 2000 + Math.random() * 1500);
     }
   }
 
   // Detects if Ctrl + C is pressed, stops the clicking, and stops listening for keyboard inputs
   if (key) {
     if (key.name === 'c' && key.ctrl === true) {
+      console.log('Clicking Cancelled!'.red);
       clearInterval(intervalId);
       process.exit();
     }
